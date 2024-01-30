035ed04918f41c69bd4b714181c09194663326cf
Your New Username <danieokandeji@gmail.com>
2024-01-30 01:36:56 +0100

    another one

--- src/components/Nextpage.jsx
+++ src/components/Nextpage.jsx
@@ -1,6 +1,6 @@
 import video from '../assets/video-recorder.png'
 import recorder from '../assets/recording.png'
-
+import unlock from '../assets/lunlock.png'
 import calenders from '../assets/calenders.png'
 const Nextpage = () => {
   return (
@@ -16,13 +16,13 @@ const Nextpage = () => {
 
 
             <div>
-                <div>
+                <div className='grid-contain'>
 
-                    <div>
+                        <div>
                             <div><img src={video} alt="" /></div>
                             <div><h5>Crystal-clear HD video</h5></div>
                             <div><p>No more pixelation or blurriness – just <br /> stunning, lifelike clarity that brings your <br /> team closer in meetings.</p></div>
-                    </div>
+                         </div>
 
                     <div>
                         <div><img src={recorder} /></div>
@@ -33,7 +33,13 @@ const Nextpage = () => {
                     <div>
                         <div><img src={calenders} alt="" /></div>
                         <div><h5>Scheduling made easy</h5></div>
-                        <div><p>Streamline your agenda with ClearLink's <br /> intuitive scheduling. Set up meetings, <br /> send invitations, and receive reminders in one place.</p></div>
+                        <div><p>Streamline your agenda with ClearLinks <br /> intuitive scheduling. Set up meetings, <br /> send invitations, and receive reminders in <br /> one place.</p></div>
+                    </div>
+
+                    <div>
+                        <div><img src={unlock} alt="" /></div>
+                        <div><h5>Bank-grade security</h5></div>
+                        <div><p>Your privacy is our priority with bank- <br />grade security protocols safeguarding <br /> your meetings and data from unwanted <br />intruders.</p></div>
                     </div>
                 </div>
             </div>

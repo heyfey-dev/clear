2cdb0e655ae88cbf0f36f957454d7bfe23efb50b
Your New Username <danieokandeji@gmail.com>
2024-01-30 15:24:45 +0100

    new

---
+++ src/components/lastpage.jsx
@@ -0,0 +1,62 @@
+
+
+const lastpage = () => {
+  return (
+    <div>
+      <section>
+        <div>
+            <div>
+                <div>
+                    <div><img src="" alt="" /></div>
+                    <div><p>ClearLink is your gateway to effortless, high- <br />quality video conferencing. Join us in shaping <br /> the future of communication!</p></div>
+                </div>
+
+                <div>
+                    <div>
+                        <div><span>Product</span></div>
+                        <div><span>Overview</span></div>
+                        <div><span>features</span></div>
+                        <div><span>Solutions</span></div>
+                        <div><span>Tutorials</span></div>
+                        <div><span>Pricinig</span></div>
+                    </div>
+                    <div>
+                         <div><span>Company</span></div>
+                         <div><span>About us </span></div>
+                         <div><span>Careers</span></div>
+                         <div><span>Press</span></div>
+                         <div><span>News</span></div>
+                         <div><span>Contact</span></div>
+                    </div>
+                    <div>
+                        <div><span>Resources</span></div>
+                        <div><span>Blog</span></div>
+                        <div><span>Events</span></div>
+                        <div><span>Hlep centre</span></div>
+                        <div><span>Turorials</span></div>
+                        <div><span>Support</span></div>
+                    </div>
+                    <div>
+                        <div><span>Legal</span></div>
+                        <div><span>Terms</span></div>
+                        <div><span>Privacy</span></div>
+                        <div><span>Cookies</span></div>
+                        <div><span>License</span></div>
+                        <div><span>Contact</span></div>
+                    </div>
+                </div>
+
+
+                <div>
+                    <div><p>Get the app</p></div>
+                    <div><img src="" alt="" /></div>
+                    <div><img src="" alt="" /></div>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default lastpage

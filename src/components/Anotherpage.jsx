88796b66bab7de21aa0b10684e0fa5348993a3b3
Your New Username <danieokandeji@gmail.com>
2024-01-30 11:37:05 +0100

    new

--- src/components/Anotherpage.jsx
+++ src/components/Anotherpage.jsx
@@ -6,17 +6,19 @@ import star4 from '../assets/star4.png'
 import star5 from '../assets/star5.png'
 import another from '../assets/another.png'
 import contents from '../assets/contents.png'
+import plussign from '../assets/plus1.png'
+import minussign from '../assets/minus.png'
 
 const Anotherpage = () => {
   return (
     <div style={{
-      backgroundColor: "var(--Gray-50, #F9FAFB)"
+      
     }}>
 
 
           <section style={{
             marginTop:"90px",
-           
+            backgroundColor: "var(--Gray-50, #F9FAFB)"
             
           }}>
                   <div className=''style={{
@@ -78,6 +80,163 @@ const Anotherpage = () => {
                       </div>
                   </div>
           </section>
+            
+
+
+
+            <main style={{
+              marginTop:'5rem'
+            }}>
+          <div className='d-flex gap-5' style={{
+            justifyContent:"space-between"
+          }}>
+                    <div>
+                      <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            color: "var(--Blue-700, #175CD3)",
+                                            
+                                          }}>Support</p></div>
+                      <div><h2>FAQs</h2></div>
+                      <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>Everything you need to know about the product <br /> and billing. Can’t find the answer you’re looking <br /> for? Please chat to our friendly team.</p></div>
+                    </div>
+
+
+                  <div>
+                                    <div style={{
+                                       backgroundColor: "var(--Gray-50, #F9FAFB)",
+                                      border: "1px solid var(--Gray-200, #EAECF0)",
+                                      borderRadius: "16px",
+                                      padding: "32px",
+                                      
+                                      
+                                    }}>
+                                          <div style={{
+                                             display: "flex",
+                                             flexDirection: "row",
+                                             justifyContent:"space-between"
+                                          }} >
+                                          <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>How many participants can join a ClearLink video conference?</p></div>
+                                          <div><img src={minussign} alt="" /></div>
+                                          </div>
+                                           <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "18px",
+                                            fontStyle: "normal",
+                                            fontWeight: "400",
+                                            
+                                          }}>ClearLink offers flexible meeting options. Depending on your subscription <br /> plan, you can host meetings with varying numbers of participants. Our plans <br /> are designed to accommodate small team collaborations and large-scale <br /> webinars, ensuring you have the right fit for your needs.</p></div>
+                                    </div>
+
+
+
+
+                      <div className='mt-4'>
+                                <div style={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  justifyContent:"space-between"
+                                  
+                                  
+                                }}>
+                                    <div> <p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>Can i use ClearLink on multiple devices</p></div>
+                                    <div><img src={plussign} alt="" /></div>
+                                </div>
+                                      <hr /> 
+                                <div style={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  justifyContent:"space-between"
+                                  
+                                  
+                                }}>
+                                  <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}> Is clearLink compatible with other video conferencing platforms?</p></div>
+                                  <div><img src={plussign} alt="" /></div>
+                                </div>
+                                    <hr />
+                                <div style={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  justifyContent:"space-between"
+                                  
+                                  
+                                }}>
+                                  <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>How does ClearLink ensure the security of my video conferences?</p></div>
+                                  <div><img src={plussign} alt="" /></div>
+                                </div>
+                                      <hr />
+                                <div style={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  justifyContent:"space-between"
+                                  
+                                  
+                                }}>
+                                  <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>Do I need to download any software to use ClearLink?</p></div>
+                                  <div><img src={plussign} alt="" /></div>
+                                </div>
+                                    <hr />
+                                <div style={{
+                                  display: "flex",
+                                  flexDirection: "row",
+                                  justifyContent:"space-between"
+                                  
+                                  
+                                }}>
+                                  <div><p style={{
+                                            fontFamily: "Inter",
+                                            fontSize: "20px",
+                                            fontStyle: "normal",
+                                            fontWeight: "600",
+                                            
+                                          }}>What kind of customer support does ClearLink provide?</p></div>
+                                  <div><img src={plussign} alt="" /></div>
+                                </div>
+                      </div>
+                  </div> 
+
+          </div>
+</main>
+
+
 
 
     </div>
